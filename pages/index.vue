6836cd8be315309a6e7367bf43c234a1cb868231
tina234 <martina.dejanovic94@gmail.com>
2021-10-09 20:49:06 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-box">
+        <div class="container">
+            <div class="title-box"> 
+                <h2>devfinder</h2>
+                <p>LIGHT</p>
+            </div>
+            <div class="search-box">
+                <input type="text" placeholder="Search github user..." v-model="user_name">
+                <span @click="fetchUser(user_name)">Search</span>
+            </div>
+            <div>
+
+                
+            </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapMutations } from 'vuex';
+
+export default {
+
+    data (){
+        return {
+            user_name: '',
+        }
+    },
+
+    computed: {
+        ...mapGetters ({
+            user: 'getUser',
+        }),
+    },
+
+    methods: {
+        ...mapActions (['fetchUser']),
+    },
+}
+</script>
+
+<style lang="sass">
+    @import "../assets/scss/home.scss";
+</style>
---
+++ store/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export const state = () => ({
+    user: {},
+})
+
+export const getters = {
+    getUser (state){
+        return state.user;
+    }
+};
+
+export const mutations = {
+    saveUser (state, user){
+        state.user =  user;
+    },
+};
+
+export const actions = {
+    async fetchUser({commit}, user_name){
+       let response = await axios.get('https://api.github.com/users/' + user_name);
+       console.log(response);
+       commit('saveUser', response.data);
+    }
+};
